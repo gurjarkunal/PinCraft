db7e9783061e3f01bbc86441c9ad922c2eb2d886
gurjarkunal <kunalgurjarkg786@gmail.com>
2024-02-16 00:48:32 +0530

    env config

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const plm = require("passport-local-mongoose");
-
-mongoose.connect("mongodb+srv://sharmakaratika57:kunal2425@pincraft.izgfmkp.mongodb.net/pin")
+mongoose.connect(process.env.MONGO)
 // mongoose.connect("mongodb://127.0.0.1:27017/pin");
 
 // Define the user schema
